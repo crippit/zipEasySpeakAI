87b0cd830d3129eda61f5846256262d1ab0d7ffb
Chris <>
2026-03-08 14:08:28 -0400

    feat: Register PWA service worker for automatic updates.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
-    import ReactDOM from 'react-dom/client'
-    import App from './App.jsx'
-    import './index.css' // <--- This import is CRITICAL
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css' // <--- This import is CRITICAL
+import { registerSW } from 'virtual:pwa-register'
 
-    ReactDOM.createRoot(document.getElementById('root')).render(
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>,
-    )
+// Automatically register and update the PWA service worker
+registerSW({ immediate: true })
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
